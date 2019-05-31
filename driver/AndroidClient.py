1dc78d871b69497c3d6ff09dffe2d88fb2f80d93
sunym06 <ufc_test@163.com>
2019-05-31 16:22:18 +0800

    尝试git push

--- driver/AndroidClient.py
+++ driver/AndroidClient.py
@@ -20,3 +20,5 @@ class AndroidClient(object):
         cls.driver = webdriver.Remote("http://localhost:4723/wd/hub", caps)
         cls.driver.implicitly_wait(10)
         return cls.driver
+
+a = AndroidClient
